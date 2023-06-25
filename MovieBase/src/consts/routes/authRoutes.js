2bb811371f3d3c034ef166ebc8d99c52a1fdb0d2
Choster <komarsergeevoch@gmail.com>
2023-06-25 20:42:11 +0300

    wrongly named folder eventually removed from remote

--- MovieBase/src/consts/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// auth routes, with sign in
-
-export const friends_route = '/friends';
-export const feed_route = '/feed';
-export const account_route = '/account';
-export const watchlist_route = '/watchlist';
-
-// MovieMatch - tinder-like feature that allows to choose movie or tv-show for a couple of people
-export const movie_match_route = '/movie_match';
